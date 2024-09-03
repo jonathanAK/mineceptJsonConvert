fa33ed673658bd85e6279d6854f10badfdb9a6f1
jonat <jonathan@amitkohn.com>
2024-09-03 10:36:29 +0300

    WIP

--- parseEvents.js
+++ parseEvents.js
@@ -4,47 +4,58 @@ const path = require('path');
 const args = process.argv.slice(2);
 const jsonDirPath = args[0]; // First argument: JSON source directory
 const outputFilePath = args[1]; // Second argument: Output file path
-
-const combineJson = () => {
-    // Determine the complete path for the output
-    const outputPath = path.resolve(outputFilePath);
-
+const combineJson = (combinedData, dirPath) => {
     // Ensure the directory exists
-    if (!fs.existsSync(jsonDirPath)) {
-        console.error("Directory does not exist:", jsonDirPath);
+    if (!fs.existsSync(dirPath)) {
+        console.error("Directory does not exist:", dirPath);
         return;
     }
 
-    const files = fs.readdirSync(jsonDirPath);
-    let combinedData = [];
-
+    const files = fs.readdirSync(dirPath);
     files.forEach(file => {
-        const filePath = path.join(jsonDirPath, file);
+        try{
+            const filePath = path.join(dirPath, file);
 
-        // Read file if it is a JSON file
-        if (path.extname(file) === '.json') {
-            const fileData = fs.readFileSync(filePath);
-            const stats = fs.statSync(filePath);
-            // Convert birthtime to Unix timestamp
-            const creationTimeUnix = Math.floor(new Date(stats.birthtime).getTime() / 1000);
-            const jsonData = JSON.parse(fileData);
-            const eventData = {
-                Severity: jsonData.Severity,
-                typeOfEvent: jsonData.typeOfEvent,
-                time: creationTimeUnix
-            };
-            // Combine json data
-            combinedData.push(eventData);
+            // Read file if it is a JSON file
+            if (path.extname(file) === '.json') {
+                const fileData = fs.readFileSync(filePath);
+                const stats = fs.statSync(filePath);
+                // Convert birthtime to Unix timestamp
+                const creationTimeUnix = Math.floor(new Date(stats.birthtime).getTime() / 1000);
+                const jsonData = JSON.parse(fileData);
+                const eventData = {
+                    Severity: jsonData.Severity,
+                    typeOfEvent: jsonData.typeOfEvent,
+                    time: creationTimeUnix
+                };
+                // Combine json data
+                combinedData.push(eventData);
+            }
+        }catch (e) {
+            console.log(`${dirPath}/${file}:\n${e}`)
         }
+
     });
 
+};
+
+
+const checkFolders = () => {
+    // Determine the complete path for the output
+    const outputPath = path.resolve(outputFilePath);
+    let combinedData = [];
     // Save combined JSON to the specified output file
     if (!fs.existsSync(path.dirname(outputPath))) {
         fs.mkdirSync(path.dirname(outputPath), { recursive: true });
     }
-    fs.writeFileSync(outputPath, JSON.stringify(combinedData, null, 4));
+    const folders = fs.readdirSync(jsonDirPath);
+    folders.forEach((dir)=>{
+        combineJson(combinedData, `${jsonDirPath}/${dir}`)
+        fs.writeFileSync(`${outputPath}/${dir}.json`, JSON.stringify(combinedData, null, 4))
+        console.log(`Combined JSON saved to ${outputPath}/${dir}.json`);
+        combinedData = [];
+    });
 
-    console.log(`Combined JSON saved to ${outputPath}`);
-};
+}
 
-combineJson();
+checkFolders();
